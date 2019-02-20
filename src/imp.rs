839ae88e3fb6085f1e6f9c4dd71e9244c0f9c5f6
rleungx <rleungx@gmail.com>
2019-02-20 16:55:59 +0800

    remove snapshot from MemoryStorageTransaction and small fix
    Signed-off-by: rleungx <rleungx@gmail.com>

--- src/imp.rs
+++ src/imp.rs
@@ -2,14 +2,16 @@ use std::collections::BTreeMap;
 use std::sync::{Arc, Mutex};
 use std::time;
 
+type Key = (Vec<u8>, u64);
+
 #[derive(Clone, Default)]
 pub struct KvTable {
     // column write <(Key, Timestamp), Timestamp>
-    write: BTreeMap<(Vec<u8>, u64), u64>,
+    write: BTreeMap<Key, u64>,
     // column data <(Key, Timestamp), Value>
-    data: BTreeMap<(Vec<u8>, u64), Vec<u8>>,
-    // column lock <Key, Timestamp>
-    lock: BTreeMap<Vec<u8>, u64>,
+    data: BTreeMap<Key, Vec<u8>>,
+    // column lock <(Key, Timestamp), Key>
+    lock: BTreeMap<Key, Vec<u8>>,
 }
 
 impl KvTable {
@@ -19,7 +21,7 @@ impl KvTable {
         key: Vec<u8>,
         ts_start_inclusive: Option<u64>,
         ts_end_inclusive: Option<u64>,
-    ) -> Option<(&(Vec<u8>, u64), &u64)> {
+    ) -> Option<(&Key, &u64)> {
         let range_start = match ts_start_inclusive {
             None => (key.clone(), 0),
             Some(ts) => (key.clone(), ts),
@@ -39,8 +41,22 @@ impl KvTable {
     }
 
     #[inline]
-    pub fn get_lock(&self, key: Vec<u8>) -> Option<u64> {
-        self.lock.get(key.as_slice()).cloned()
+    pub fn get_lock(
+        &self,
+        key: Vec<u8>,
+        ts_start_inclusive: Option<u64>,
+        ts_end_inclusive: Option<u64>,
+    ) -> Option<(&Key, &Vec<u8>)> {
+        let range_start = match ts_start_inclusive {
+            None => (key.clone(), 0),
+            Some(ts) => (key.clone(), ts),
+        };
+        let range_end = match ts_end_inclusive {
+            None => (key.clone(), std::u64::MAX),
+            Some(ts) => (key.clone(), ts),
+        };
+        let mut r = self.lock.range(range_start..=range_end);
+        r.next_back()
     }
 
     #[inline]
@@ -50,8 +66,9 @@ impl KvTable {
     }
 
     #[inline]
-    pub fn put_lock(&mut self, key: Vec<u8>, start_ts: u64) {
-        let _ = self.lock.insert(key, start_ts);
+    pub fn put_lock(&mut self, key: Vec<u8>, start_ts: u64, value: Vec<u8>) {
+        let map_key = (key, start_ts);
+        let _ = self.lock.insert(map_key, value);
     }
 
     #[inline]
@@ -61,8 +78,14 @@ impl KvTable {
     }
 
     #[inline]
-    pub fn erase_lock(&mut self, key: Vec<u8>) {
-        let _ = self.lock.remove(&key);
+    pub fn erase_lock(&mut self, key: Vec<u8>, commit_ts: u64) {
+        let l = self.lock.clone();
+
+        for (map_key, _) in l.iter() {
+            if key.as_slice() == map_key.0.as_slice() && map_key.1 <= commit_ts {
+                let _ = self.lock.remove(&map_key);
+            }
+        }
     }
 }
 
@@ -86,7 +109,6 @@ impl crate::Store for MemoryStorage {
         MemoryStorageTransaction {
             start_ts: get_timestamp(),
             data: self.data.clone(),
-            snapshot: self.data.lock().unwrap().clone(),
             writes: vec![],
         }
     }
@@ -98,27 +120,26 @@ struct Write(Vec<u8>, Vec<u8>);
 pub struct MemoryStorageTransaction {
     start_ts: u64,
     data: Arc<Mutex<KvTable>>,
-    snapshot: KvTable,
     writes: Vec<Write>,
 }
 
 impl crate::Transaction for MemoryStorageTransaction {
     fn get(&self, key: Vec<u8>) -> Option<Vec<u8>> {
         loop {
-            if let Some(ts) = self.snapshot.get_lock(key.clone()) {
-                if ts < self.start_ts {
+            let snapshot = self.data.lock().unwrap().clone();
+
+            if let Some(r) = snapshot.get_lock(key.clone(), None, Some(self.start_ts)) {
+                if (*r.0).1 < self.start_ts {
+                    // Check for locks that signal concurrent writes.
+                    // TODO: BackOffAndMaybeCleanupLock
                     continue;
                 }
             }
-            // Check for locks that signal concurrent writes.
-            // TODO: BackoffAndMaybeCleanupLock
 
             // Find the latest write below our start timestamp.
-            let (_, data_ts) = self
-                .snapshot
-                .get_write(key.clone(), None, Some(self.start_ts))?;
-                
-            let value = self.snapshot.get_data(key.clone(), *data_ts);
+            let (_, data_ts) = snapshot.get_write(key.clone(), None, Some(self.start_ts))?;
+            let value = snapshot.get_data(key.clone(), *data_ts);
+
             return value;
         }
     }
@@ -134,6 +155,7 @@ impl crate::Transaction for MemoryStorageTransaction {
         if !self.prewrite(primary, primary) {
             return false;
         }
+
         for w in secondaries {
             if !self.prewrite(w, primary) {
                 return false;
@@ -144,8 +166,10 @@ impl crate::Transaction for MemoryStorageTransaction {
         let commit_ts = get_timestamp();
         let mut kv_data = self.data.lock().unwrap();
 
-        if let Some(lock_ts) = kv_data.get_lock(primary.0.clone()) {
-            if lock_ts != self.start_ts {
+        if let Some(r) =
+            kv_data.get_lock(primary.0.clone(), Some(self.start_ts), Some(self.start_ts))
+        {
+            if (*r.0).1 != self.start_ts {
                 // Lock is not found
                 return false;
             }
@@ -153,13 +177,14 @@ impl crate::Transaction for MemoryStorageTransaction {
             // Lock is not found
             return false;
         }
+
         kv_data.put_write(primary.0.clone(), commit_ts, self.start_ts);
-        kv_data.erase_lock(primary.0.clone());
+        kv_data.erase_lock(primary.0.clone(), commit_ts);
 
         // Second phase: write out write records for secondary cells.
         for w in secondaries {
             kv_data.put_write(w.0.clone(), commit_ts, self.start_ts);
-            kv_data.erase_lock(w.0.clone());
+            kv_data.erase_lock(w.0.clone(), commit_ts);
         }
 
         true
@@ -171,6 +196,7 @@ impl MemoryStorageTransaction {
     fn prewrite(&self, w: &Write, primary: &Write) -> bool {
         let mut kv_data = self.data.lock().unwrap();
         let latest_write = kv_data.get_write(w.0.clone(), Some(self.start_ts), None);
+
         if latest_write.is_some() {
             let (k, _) = latest_write.unwrap();
             if (*k).1 >= self.start_ts {
@@ -178,15 +204,16 @@ impl MemoryStorageTransaction {
                 return false;
             }
         }
-        if let Some(lock_ts) = kv_data.get_lock(w.0.clone()) {
-            if lock_ts != self.start_ts {
+
+        if let Some(r) = kv_data.get_lock(w.0.clone(), None, None) {
+            if (*r.0).1 != self.start_ts {
                 // ... or locks at any timestamp.
                 return false;
             }
         }
 
         kv_data.put_data(w.0.clone(), self.start_ts, w.1.clone());
-        kv_data.put_lock(primary.0.clone(), self.start_ts);
+        kv_data.put_lock(w.0.clone(), self.start_ts, primary.0.clone());
 
         true
     }
